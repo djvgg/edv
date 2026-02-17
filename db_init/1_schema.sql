ae80afe2a6d8aed8ba31ce0297900dadb6358db6
Engin <inf4296@-worms.de>
2026-02-17 10:24:25 +0100

    Added Docker

---
+++ db_init/1_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    birth_date DATE NOT NULL,
+    club VARCHAR(100) NOT NULL,
+    gender CHAR(1) CHECK (gender IN ('m','w')),
+    weight NUMERIC(5,2),        -- kg, up to 999.99
+    valid BOOLEAN DEFAULT FALSE, -- is membership valid
+    paid BOOLEAN DEFAULT FALSE   -- has membership fee been paid
+);
